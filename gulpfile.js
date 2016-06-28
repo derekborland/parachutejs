9b216b155271f2d85edccd4cfbad688496a5fac5
DerekB <derek@motionstrand.com>
2016-06-28 10:45:33 -0700

    initial fil/dir setup

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var header = require('gulp-header');
+var rename = require('gulp-rename');
+var pkg = require('./package.json');
+
+
+// file header helper function
+function fileHeader() {
+	return[    
+    '/*! ' + pkg.name + ' ' + pkg.version + ' | https://github.com/derekborland/parachutejs.git */\n' 
+	].join('\n');
+}
+
+
+// copyright helper function
+function copyrightYear(creationDate) {
+	var now = new Date().getFullYear();
+	if(creationDate == now) {
+		return '2016';
+	}
+	return creationDate + '-' + now;
+}
+
+
+// js files
+var jsFiles = ['src/**/*.js'];
+
+
+// build
+gulp.task('build', function() {
+	gulp.src([
+		'src/parachute.js'
+	])
+	.pipe(header(fileHeader('Parachute JS')))
+	.pipe(rename({
+		suffix: '-' + pkg.version
+	}))
+	.pipe(gulp.dest('lib/'));
+});
+
+
+// build min
+gulp.task('build-min', function() {
+	gulp.src([
+		'src/parachute.js'
+	])
+	.pipe(header(fileHeader('Parachute JS')))
+	.pipe(rename({
+		suffix: '-' + pkg.version + '.min'
+	}))
+	.pipe(uglify({
+		preserveComments: 'some'
+	}))
+	.pipe(gulp.dest('lib/'));
+});
+
+
+// watch
+gulp.task('watch', function() {
+	gulp.watch(jsFiles, ['build', 'build-min']);
+});
+
+
+// default
+gulp.task('default', ['build', 'build-min', 'watch']);
---
+++ src/parachute.js
@@ -0,0 +1,5 @@
+;(function($, window, document, undefined){
+	
+	
+	
+})(jQuery, window, document);
